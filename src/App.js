84b4882f5f0c8907dd508e32decc2823b80eaec8
sonny <dhsmf4245@gmail.com>
2024-06-17 12:00:01 +0900

    feat:  load kakao map api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Join from './join/Join.js';
 import RegisterForm from './join/RegisterFrom.js';
 import LoginPage from './login/Login.js';
-import { Link } from 'react-router-dom';
+import Location from './location/location.js';
 
 function App() {
   return (
+    
   
     <Router>
       <Routes>
         {/* <Route path='/join' element={<Join />} /> */}
         <Route path='/register' element={<RegisterForm />} />
         <Route path='/login' element ={<LoginPage />} />
+        <Route path='/location' element = {<Location />} />
       </Routes>
-
     </Router>
   );
 }
